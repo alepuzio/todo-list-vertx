83e2f096c9c080ce7c9218cf8e9924982757c5f1
Marco <m.rotondi@ngi.it>
2018-01-08 23:16:39 +0100

    Add Sql Connection Add pom dependency for Jdbc Client

--- src/main/java/io/vertx/mokabyte/datastore/DataStoreVerticle.java
+++ src/main/java/io/vertx/mokabyte/datastore/DataStoreVerticle.java
@@ -2,6 +2,8 @@ package io.vertx.mokabyte.datastore;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +12,9 @@ public class DataStoreVerticle extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> storeFeature) throws Exception {
+        final SQLClient sqlClient = JDBCClient.create(getVertx(), DataSourceConfig.initDataSource(config()));
+
+
         logger.info("Successful start DataStore");
     }
 
