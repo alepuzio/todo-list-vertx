49d4ee04e35536a979ed1c52ba499c0b389d7d93
Marco <m.rotondi@ngi.it>
2018-01-18 00:09:51 +0100

    Init Test DataStore

--- src/test/java/TodoDataStoreVerticleTestCase.java
+++ src/test/java/TodoDataStoreVerticleTestCase.java
@@ -3,14 +3,15 @@ import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.mokabyte.datastore.DataSourceConfig;
 import io.vertx.mokabyte.datastore.DataStoreVerticle;
 import io.vertx.mokabyte.model.TodoModel;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
+import org.flywaydb.core.Flyway;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+
+import javax.sql.DataSource;
 
 @RunWith(VertxUnitRunner.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -20,22 +21,59 @@ public class TodoDataStoreVerticleTestCase {
 
     private TodoModel todoModel;
 
+    private DataSource dataSource = null;
+
     @BeforeClass
     public static void init(final TestContext context) {
+        final JsonObject properties = new JsonObject();
+        properties.put("datasource.driver", "org.h2.Driver");
+        properties.put("datasource.url", "jdbc:h2:mem:todoDb");
+        properties.put("datasource.user", "sa");
+        properties.put("datasource.password", "");
+
+        final DeploymentOptions options = new DeploymentOptions().setConfig(properties);
+        initDB(properties);
+
         vertx = Vertx.vertx();
-        final DeploymentOptions options = new DeploymentOptions()
-                .setConfig(new JsonObject().put("datasource.driver", "org.h2.Driver"))
-                .setConfig(new JsonObject().put("datasource.url", "jdbc:h2:file:c:/development/var/storage/h2/todoDb;DB_CLOSE_ON_EXIT=FALSE"))
-                .setConfig(new JsonObject().put("datasource.user", "sa"))
-                .setConfig(new JsonObject().put("datasource.password", ""));
 
         // We pass the options as the second parameter of the deployVerticle method.
         vertx.deployVerticle(DataStoreVerticle.class.getName(), options, context.asyncAssertSuccess());
     }
 
+    private static void initDB(JsonObject properties) {
+        final Flyway flyway = new Flyway();
+        flyway.setDataSource(DataSourceConfig.initDataSource(properties));
+        flyway.migrate();
+    }
+
     @Before
     public void setUp() {
         todoModel = TestUtil.createTestModel();
+    }
+
+    @Test
+    public void createNewContent() {
+
+    }
+
+    @Test
+    public void readContent() {
+
+    }
+
+    @Test
+    public void readAllContent() {
+
+    }
+
+    @Test
+    public void updateContent() {
+
+    }
+
+    @Test
+    public void whenIsLastCallDelete() {
+
     }
 
     @AfterClass
